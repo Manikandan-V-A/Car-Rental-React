142b5db20cbe2dc8022289120681c811f04c796c
Manikandan-V-A <131941431+Manikandan-V-A@users.noreply.github.com>
2024-08-15 01:12:56 +0530

    commit
    commit

---
+++ src/assets/data/carData.js
@@ -0,0 +1,157 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/car-data-1.png";
+import img02 from "../all-images/cars-img/car-data-2.png";
+import img03 from "../all-images/cars-img/car-data-3.png";
+import img04 from "../all-images/cars-img/car-data-4.png";
+import img05 from "../all-images/cars-img/car-data-5.png";
+import img06 from "../all-images/cars-img/car-data-6.png";
+import img07 from "../all-images/cars-img/car-data-7.png";
+import img08 from "../all-images/cars-img/car-data-8.png";
+import img09 from "../all-images/cars-img/car-data-9.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Lamborghini",
+    rating: 112,
+    carName: "Lamborghini Aventador",
+    imgUrl: img01,
+    model: "Model 3",
+    price: "₹9,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "BMW",
+    rating: 102,
+    carName: "BMW M5",
+    imgUrl: img02,
+    model: "Model-2022",
+    price: "₹6,999",
+    speed: "₹20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "Nissan",
+    rating: 132,
+    carName: "Nissan GTR",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: "₹7,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Bugatti",
+    rating: 102,
+    carName: "Bugatti Chiron",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: "₹9,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Toyota",
+    rating: 94,
+    carName: "Toyota Fortuner",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: "₹4,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes",
+    rating: 119,
+    carName: "Mercedes Benz GT-S",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: "₹5,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Rolls Royce",
+    rating: 82,
+    carName: "Rolls Royce Colorado",
+    imgUrl: img07,
+    model: "Model 3",
+    price: "₹9,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Audi",
+    rating: 52,
+    carName: "Audi RS6",
+    imgUrl: img08,
+    model: "Model 3",
+    price: "₹8,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+  {
+    id: 9,
+    brand: "BMW",
+    rating: 52,
+    carName: "BMW i8",
+    imgUrl: img09,
+    model: "Model 3",
+    price: "₹8,999",
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
