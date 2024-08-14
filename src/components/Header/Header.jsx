76643888b8a5ba0a6e2218d9c9ae2b1e264c7941
Manikandan-V-A <131941431+Manikandan-V-A@users.noreply.github.com>
2024-08-15 01:32:06 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                  <i class="ri-phone-fill"></i> +91 9876543219
                 </span>
               </div>
             </Col>
